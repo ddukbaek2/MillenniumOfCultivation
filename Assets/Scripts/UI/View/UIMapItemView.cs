0f0b7d087c6aa8d87d226f9b188cb27137d8db4a
ddukbaek2 <ddukbaek2@gmail.com>
2025-12-23 11:59:30 +0900

    작업.

---
+++ Assets/Scripts/UI/View/UIMapItemView.cs
@@ -0,0 +1,25 @@
+using Crockhead.Unity;
+using Crockhead.Unity.UI;
+
+
+namespace MillenniumOfCultivation.UI
+{
+	/// <summary>
+	/// 맵 뷰.
+	/// </summary>
+	[AssetPath("Assets/Resources/UI/UIMapItemView.prefab", AssetPathType.Resources)]
+	public class UIMapItemView : UIItemView
+	{
+		#region INSPECTOR
+		//[SerializeField] private Image m_LabelView;
+		#endregion
+
+		/// <summary>
+		/// 생성됨.
+		/// </summary>
+		protected override void OnCreate()
+		{
+			base.OnCreate();
+		}
+	}
+}
